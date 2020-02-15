51c2f953bae38b887665b86215ce8d00e1585519
TXacs <60869411+TXacs@users.noreply.github.com>
2020-02-15 10:24:18 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,4 +1,11 @@
 import tensorflow as tf
-a = tf.constant([2.0, 3.0])
-with tf.compat.v1.Session() as sess:
-  print(sess.run(a))
+#a = tf.constant([2.0, 3.0])
+#with tf.compat.v1.Session() as sess:
+#  print(sess.run(a))
+g=tf.Graph()
+with g.as_default():
+    v_1 = tf.constant([1, 2, 3, 4])
+    v_2 = tf.constant([2, 3, 4, 5])
+    v_add = tf.add(v_1, v_2)
+    sess=tf.compat.v1.Session(graph=g)
+    print(sess.run(v_add))
